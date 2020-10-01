4f5e1cc05760f9ea6970f40549e10cb4db5b00fd
Ksenya TORKOVA <ksenyatorkova@o-s-i.org>
2020-10-01 15:41:17 +0300

    Добавляет пропсы и их валидацию

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,10 +1,17 @@
 import React from "react";
+import PropTypes from "prop-types";
 import MainPage from "../main-page/main-page";
 
-const App = () => {
+const App = (props) => {
+  const {placesAmount} = props;
+
   return (
-    <MainPage />
+    <MainPage placesAmount = {placesAmount} />
   );
+};
+
+App.propTypes = {
+  placesAmount: PropTypes.number.isRequired,
 };
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,7 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/app/app";
 
+const Settings = {
+  PLACES_AMOUNT: 312,
+};
+
 ReactDOM.render(
-    <App />,
+    <App placesAmount = {Settings.PLACES_AMOUNT} />,
     document.querySelector(`#root`)
 );
